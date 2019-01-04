b90f099328f283e4029f7254212f586514863838
Taras <taraskim@wknown.com>
2019-01-04 11:34:32 -0500

    initail comit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
     {
       $events = Event::take(3)->orderBy('date')->get();
       $photos = Photo::take(8)->latest()->get();
-      $menus = Menu::with('items')->latest()->get();
+      $menus = Menu::with('items')->orderBy('created_at', 'ASC')->get();
 
       return view('index',  compact('menus'), compact('items'))->withEvents($events)->withPhotos($photos);
     }
