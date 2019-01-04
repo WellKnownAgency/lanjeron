140bdd8ece49f8d41fd20ea7034adf55dead30f7
anton <anton.likhnekevich@gmail.com>
2019-01-04 10:46:50 -0500

    delete

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -61,4 +61,11 @@ class MenuController extends Controller
    return redirect()->route('menus.index');
 }
 
+public function destroy($id)
+{
+    $menu = Menu::findOrFail($id);
+    $menu->delete();
+    return redirect()->route('menus.index');
+}
+
 }
