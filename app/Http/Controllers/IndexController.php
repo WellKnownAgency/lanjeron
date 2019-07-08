472de493c59d7e35a6a45c19604a180715e6c23c
anton <anton.likhnekevich@gmail.com>
2019-07-08 10:35:15 -0400

    test page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,4 +19,13 @@ class IndexController extends Controller
 
       return view('index',  compact('menus'), compact('items'))->withEvents($events)->withPhotos($photos);
     }
+
+    public function test()
+    {
+      $events = Event::take(3)->orderBy('date')->get();
+      $photos = Photo::take(8)->latest()->get();
+      $menus = Menu::with('items')->orderBy('created_at', 'ASC')->get();
+
+      return view('test',  compact('menus'), compact('items'))->withEvents($events)->withPhotos($photos);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', 'IndexController@index');
+Route::get('test', 'IndexController@test');
 
 Route::middleware('auth:web')->group(function () {
   Route::get('/admin', function () {
